7f01ccc4769e5af991457e3d186dc91d9039601e
pick2510 <dominik.strebel@gmail>
2018-05-14 15:54:34 +0200

    Corrected issue if 254 appears in measurements...

--- get_soundings.py
+++ get_soundings.py
@@ -1,11 +1,15 @@
 import requests
 from fortranformat import FortranRecordReader
 import datetime
+import re
 import pandas as pd
+import sys
 
 
 class Sounding(object):
-    COLUMNS = ["TYPE","PRESSURE", "HEIGHT", "TEMP", "DEWPT", "WINDDIR", "WINDSPD"]
+    COLUMNS = ["TYPE", "PRESSURE", "HEIGHT",
+               "TEMP", "DEWPT", "WINDDIR", "WINDSPD"]
+
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
 
@@ -17,6 +21,7 @@ class Parser(object):
     IDENT2 = FortranRecordReader("(i7,10x,a4,14x,i7,5x,a2)")
     VALS = FortranRecordReader("(7i7)")
     LINEIDENT = FortranRecordReader("(i7)")
+    SPLIT_REGEX = re.compile("^    254", re.M)
 
     def __init__(self, bdate, edate, stationID):
         self.bdate = bdate
@@ -32,7 +37,8 @@ class Parser(object):
             print("ERROR, Something is wrong in your request")
             print(req.text)
             return
-        content = ["    254" + x for x in req.text.split("    254")][1:]
+        content  = ["    254" + x for x in re.split(self.SPLIT_REGEX, req.text)][1:]
+        #content = ["    254" + x for x in req.text.split("    254")][1:]
         for snd in content:
             snd_obj = Sounding()
             data = []
@@ -40,8 +46,11 @@ class Parser(object):
                 linecode = self.LINEIDENT.read(line)
                 if linecode[0] == 254:
                     header = self.HEADER.read(line)
-                    date = datetime.datetime.strptime("{}-{}-{} {}:00:00".format(
-                        header[4], header[3].strip(), header[2], header[1]), "%Y-%b-%d %H:%M:%S")
+                    try:
+                        date = datetime.datetime.strptime("{}-{}-{} {}:00:00".format(
+                            header[4], header[3].strip(), header[2], header[1]), "%Y-%b-%d %H:%M:%S")
+                    except ValueError:
+                        print("Something has gone wrong with datetime parsing....")
                     snd_obj.datetime = date
                 elif linecode[0] == 1:
                     ident = self.IDENT.read(line)
@@ -49,7 +58,8 @@ class Parser(object):
                         1], ident[2], ident[3], ident[4], ident[5], ident[6], ident[7], ident[8]
                 elif linecode[0] == 2:
                     checks = self.VALS.read(line)
-                    snd_obj.hydro, snd_obj.mxwd, snd_obj.tropl, snd_obj.lines, snd_obj.tindex, snd_obj.source = checks[1], checks[2], checks[3], checks[4], checks[5], checks[6]
+                    snd_obj.hydro, snd_obj.mxwd, snd_obj.tropl, snd_obj.lines, snd_obj.tindex, snd_obj.source = checks[
+                        1], checks[2], checks[3], checks[4], checks[5], checks[6]
                 elif linecode[0] == 3:
                     ident2 = self.IDENT2.read(line)
                     snd_obj.staid, snd_obj.sonde, snd_obj.wsunits = ident2[1], ident2[2], ident2[3]
@@ -76,10 +86,9 @@ for snd in content:
        currentline = lineident.read(line)
 """
 
-p = Parser("2002060600", "2002060610", "10868")
+p = Parser("2002060600", "2002070810", "10868")
+print(p.url)
 p.parse()
-# print(p.parse())
+snd = p.soundings
 
-
-#p = Parser("FAKE", "news", "122445")
-# p.parse()
+print(snd)
